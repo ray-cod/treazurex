5b8d41caa101041b239471c86622beb30d0e2496
ray-cod <raimilassissi8@gmail.com>
2025-07-08 14:45:58 +0200

    Created postgres connection file.

---
+++ server/src/config/db.js
@@ -0,0 +1,41 @@
+// db.js
+const { Pool } = require("pg");
+require('dotenv').config()
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_PASSWORD,
+  password: process.env.DB_USER,
+  port: process.env.DB_PORT,
+});
+
+// Create user table
+const createUsersTableQuery = `
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  email VARCHAR(150) UNIQUE NOT NULL,
+  password TEXT NOT NULL,
+  phone VARCHAR(20),
+  gender VARCHAR(10) CHECK (gender IN ('male', 'female', 'other')),
+  role VARCHAR(20) DEFAULT 'customer',
+  is_verified BOOLEAN DEFAULT false,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+`;
+
+(async () => {
+  try {
+    await pool.query(createUsersTableQuery);
+    console.log("'users' table created successfully.");
+  } catch (err) {
+    console.error("Error creating 'users' table:", err.message);
+  } finally {
+    await pool.end(); // Close the DB connection
+  }
+})();
+
+module.exports = pool;
