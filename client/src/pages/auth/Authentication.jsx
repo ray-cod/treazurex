123b3c03ebc316b9e9eb2c21f5a98774985a3b5e
ray-cod <raimilassissi8@gmail.com>
2025-07-12 18:42:44 +0200

    reduced amount of CSS.

--- client/src/pages/auth/Authentication.jsx
+++ client/src/pages/auth/Authentication.jsx
@@ -6,12 +6,12 @@ const Authentication = () => {
   const { handleGoogleLogin, handleFacebookLogin } = useAuthStore();
 
   return (
-    <section className="auth-section dark:bg-gray-950 min-h-screen dark:text-white">
+    <section className="auth-section bg-cover bg-right min-h-screen dark:text-white">
       <AuthHeader />
       <main className="auth-main flex flex-col items-center">
         <h1 className="text-3xl mb-4 mt-6">Welcome to Treazurex</h1>
         <section className="w-full max-w-5xl px-4">
-          <ul className="login-register-switch flex gap-6 pb-2 border-b">
+          <ul className="login-register-switch flex gap-6 pb-2 border-b border-[rgba(255,255,255,0.274)]">
             <li>
               <NavLink
                 to="login"
