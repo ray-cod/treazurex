ae56f9d3f4c6606dff34c12f9830e3b9a0f14e21
ray-cod <raimilassissi8@gmail.com>
2025-08-07 21:41:43 +0200

    Fixed login HTTP-only cookie.

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -69,7 +69,7 @@ const authController = {
       res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production", // true in production
-        sameSite: "Strict",
+        sameSite: "None",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       });
 
