ac708a8e8ec8fbcef62f9ab75f45c3058a890812
ray-cod <raimilassissi8@gmail.com>
2025-08-07 22:54:15 +0200

    Added test route.

--- server/src/routes/apiRoutes.js
+++ server/src/routes/apiRoutes.js
@@ -6,4 +6,9 @@ const apiController = require("../controllers/apiController");
 router.get("/product", apiController.getAllProducts);
 router.get("/product/:id", apiController.getProductById);
 
+// Test route
+router.get("/test", (req, res) => {
+  res.send("Hello API");
+});
+
 module.exports = router;
