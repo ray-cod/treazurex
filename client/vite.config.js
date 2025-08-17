b6a8791373c4ea7fc346eae6fb5d636c8536f349
ray-cod <raimilassissi8@gmail.com>
2025-08-17 17:15:00 +0200

    Fixed base path config.

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: process.env.VITE_BASE_PATH,
-})
+  plugins: [react(), tailwindcss()],
+  base: import.meta.env.VITE_BASE_PATH,
+});
